0acb80cda3df5ea096ab25ebe0659a352dc0f4b3
heliobarolo0586 <heliobarolo0586@gmail.com>
2022-07-13 15:26:24 -0300

    Jogo das portas Next.js e React

---
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -0,0 +1,56 @@
+import styles from '../../../styles/Jogo.module.css'
+import { useEffect, useState } from "react"
+import Porta from "../../../components/Porta"
+import { criarPortas, atualizarPortas } from "../../../functions/porta"
+import Link from '../../../node_modules/next/link'
+import { useRouter } from '../../../node_modules/next/router'
+
+
+export default function Jogo() {
+  const router = useRouter()
+
+  const [valido, setValido] = useState(false)
+  const [portas, setPortas] = useState([])
+
+  useEffect(() => {
+    const portas = +router.query.portas
+    const temPresente = +router.query.temPresente
+
+    const qtdePortasValidas = portas >= 3 && portas <= 50
+    const temPresenteValido = temPresente >= 1 && temPresente <= portas
+
+    setValido(qtdePortasValidas && temPresenteValido)
+  },[portas])
+
+  useEffect(() => {
+    const portas = +router.query.portas
+    const temPresente = +router.query.temPresente
+    setPortas(criarPortas(portas, temPresente))
+  },[router?.query])
+
+  router.query.portas
+  router.query.temPresente
+
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return <Porta key={porta.numero} value={porta}
+        onChange={
+          novaPorta => setPortas(atualizarPortas(portas, novaPorta))
+        } />
+    })
+  }
+
+  return (
+    <div id={styles.jogo}>
+      <div className={styles.portas}>
+        {valido ? renderizarPortas() : <h2>Valores Inválidos</h2> }
+      </div>
+
+      <div className={styles.botoes}>
+       <Link href="/">
+          <button>reiniciar Jogo</button>
+       </Link>
+      </div>
+    </div>
+  )
+}
